41a83b5ff6198539775fbf6fc4917139ce9a18fb
Carlos V. Quezada <carlosvquezada@yahoo.com>
2022-01-07 15:33:34 -0500

    Finished Tutorial

--- src/index.js
+++ src/index.js
@@ -25,9 +25,7 @@ function Square(props) {
   );
 }
 
-
 class Board extends React.Component {
-
   renderSquare(i) {
     return (
       <Square
@@ -40,7 +38,6 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -68,12 +65,13 @@ class Game extends React.Component {
       history: [{
       squares: Array(9).fill(null),
     }],
+    stepNumber: 0,
     xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length -1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -84,14 +82,22 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+      })
+    }
+
 
   render() {
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move)=> {
@@ -99,7 +105,7 @@ class Game extends React.Component {
         'Go to move #' + move :
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
@@ -107,7 +113,7 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = 'Winer: ' + winner;
+      status = 'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
