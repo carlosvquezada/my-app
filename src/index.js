bbac6a02c1273135a3a32028f2f838905db7d1a2
Carlos V. Quezada <carlosvquezada@yahoo.com>
2022-01-07 18:30:19 -0500

    FunctionalComponents

--- src/index.js
+++ src/index.js
@@ -13,65 +13,56 @@ function Square({value, onClick}) {
 }
 
 
-function Board(props){
+function Game(){
+  const [ squares, setSquares ] = useState(Array(9).fill(null));
+  const [ isXNext, setIsXNext ] = useState(true);
+  const winner = calculateWinner(squares);
+  const [ history, setHistory ] = useState();
+  const [ current, setCurrent ] = useState();
+
+
+
+
 
   function renderSquare(i) {
     return <Square 
       value={squares[i]}
       onClick={() => {
-        squares[i] = 'X';
+        const nextSquares = squares.slice();
+        nextSquares[i] = isXNext ? "X" : "O";
+        setSquares(nextSquares);
+        setIsXNext(!isXNext);
       }}
     />;
   }
 
   return (
-      <div>
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
+    <div className="game">
+      <div className="game-board">
+        <div>
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
 
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
 
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
-      </div>
-    );
-
-}
-
-
-
-function Game() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
         </div>
       </div>
+    </div>
     );
-
-
 }
+
 
 
 ReactDOM.render(
